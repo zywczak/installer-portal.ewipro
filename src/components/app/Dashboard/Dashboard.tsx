271e275da86f0f0f35caba9f5d0daf08e2a01eb2
zywczak <123404938+zywczak@users.noreply.github.com>
2026-01-15 13:10:48 +0100

    Update Dashboard.tsx

--- src/components/app/Dashboard/Dashboard.tsx
+++ src/components/app/Dashboard/Dashboard.tsx
@@ -34,13 +34,13 @@ const Dashboard: React.FC<DashboardProps> = ({ isMobile }) => {
           gap: 1,
         }}
       >
-         <Box
-        sx={{
-          flexGrow: 1,
-          minWidth: showDownload ? "500px" : "0",
-          width: showDownload ? "auto" : "100%",
-        }}
-      >
+        <Box
+          sx={{
+            flexGrow: 1,
+            minWidth: showDownload ? "500px" : "0",
+            width: showDownload ? "auto" : "100%",
+          }}
+        >
           <FinanceTiles />
         </Box>
 
@@ -92,21 +92,48 @@ const Dashboard: React.FC<DashboardProps> = ({ isMobile }) => {
             textTransform: "none",
             display: "flex",
             alignItems: "center",
-            "&:hover": { bgcolor: "transparent" },
+            position: "relative",
+            "&:hover": {
+              bgcolor: "transparent",
+              "& .see-all-text": {
+                opacity: 1,
+                transform: "translateX(-100%)",
+                pointerEvents: "auto",
+              },
+            },
           }}
         >
           <Typography
             variant="caption"
             fontWeight="800"
-            sx={{ fontSize: "0.9rem" }}
+            className="see-all-text"
+            sx={{
+              backgroundColor: "#E8F5E9",
+              fontSize: "0.9rem",
+              boxSizing: "border-box",
+              padding: "4px 8px",
+              position: "absolute",
+              right: "-520%",
+              opacity: 0,
+              whiteSpace: "nowrap",
+              pointerEvents: "none",
+              transform: "translateX(-90%)",
+              transition: "opacity 0.2s ease, transform 0.25s ease",
+            }}
           >
             {t("views.dashboard.projectList.seeAll")}
           </Typography>
 
           <ArrowForwardIos
-            sx={{ fontSize: "2rem", fontWeight: "bold", display: "flex", alignItems: "center" }}
+            sx={{
+              fontSize: "1.6rem",
+              fontWeight: "bold",
+              display: "flex",
+              alignItems: "center",
+            }}
           />
         </Button>
+
       </Box>
 
       <ProjectsView isMobile={isMobile} ongoingOnly sort="projectIDASC" showAddButton={false} stickyFooter={false} />
